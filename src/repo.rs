9bf54ffbbd1e75f69562daa510c3fb1b387a01cb
dlzht <dlzht@protonmail.com>
2025-04-22 00:45:24 +0800

    feat: unit test for repo type

--- src/repo.rs
+++ src/repo.rs
@@ -19,3 +19,36 @@ impl Default for RepoType {
     RepoType::Model
   }
 }
+
+mod test {
+  use std::assert_matches::assert_matches;
+
+  use crate::repo::RepoType;
+
+  #[test]
+  fn test_serde_repo_type() {
+    let repo_type = RepoType::Dataset;
+    let json = serde_json::to_string(&repo_type);
+    assert_matches!(json, Ok(v) if v == "\"dataset\"");
+
+    let json = "\"dataset\"";
+    let sdk = serde_json::from_str::<RepoType>(json);
+    assert_matches!(sdk, Ok(RepoType::Dataset));
+
+    let repo_type = RepoType::Space;
+    let json = serde_json::to_string(&repo_type);
+    assert_matches!(json, Ok(v) if v == "\"space\"");
+
+    let json = "\"space\"";
+    let sdk = serde_json::from_str::<RepoType>(json);
+    assert_matches!(sdk, Ok(RepoType::Space));
+
+    let repo_type = RepoType::Model;
+    let json = serde_json::to_string(&repo_type);
+    assert_matches!(json, Ok(v) if v == "\"model\"");
+
+    let json = "\"model\"";
+    let sdk = serde_json::from_str::<RepoType>(json);
+    assert_matches!(sdk, Ok(RepoType::Model));
+  }
+}
