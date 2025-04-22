5615ce7ab94c6b5362a94fd20c3ea995a45c2ae7
dlzht <dlzht@protonmail.com>
2025-04-22 23:10:43 +0800

    feat: get_model(s) usage examples

---
+++ examples/get_model.rs
@@ -0,0 +1,17 @@
+use hugging_face_client::api::GetModelReq;
+use hugging_face_client::client::{Client, ClientOption};
+use hugging_face_client::errors::Result;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+  let access_token = std::env::var("HUGGING_FACE_TOKEN").unwrap();
+  let access_proxy = std::env::var("HUGGING_FACE_PROXY").unwrap();
+  let option = ClientOption::new(access_token).proxy(access_proxy);
+  let client = Client::new(option)?;
+
+  // get models
+  let get_model_req = GetModelReq::new("microsoft/bitnet-b1.58-2B-4T");
+  let res = client.get_model(get_model_req).await?;
+  println!("Get Model: {:#?}", res);
+  Ok(())
+}
